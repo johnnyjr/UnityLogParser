e15754ea06c3984c70734690259e4e509a2bbf19
johnnyjr <johnnyjr@tomcats.se>
2020-12-22 10:41:22 +0100

    Initial commit

---
+++ Forms/frmMain.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace UnityLogParser
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtStacktrace = new System.Windows.Forms.TextBox();
+            this.chkHideSystem = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstExceptions = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(16, 29);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(230, 20);
+            this.txtFile.TabIndex = 1;
+            this.txtFile.TextChanged += new System.EventHandler(this.txtFile_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(252, 29);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(70, 20);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Enabled = false;
+            this.btnRead.Location = new System.Drawing.Point(328, 29);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 20);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtStacktrace);
+            this.groupBox1.Controls.Add(this.chkHideSystem);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lstExceptions);
+            this.groupBox1.Location = new System.Drawing.Point(12, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(982, 369);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // txtStacktrace
+            // 
+            this.txtStacktrace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStacktrace.Location = new System.Drawing.Point(6, 197);
+            this.txtStacktrace.Multiline = true;
+            this.txtStacktrace.Name = "txtStacktrace";
+            this.txtStacktrace.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStacktrace.Size = new System.Drawing.Size(970, 166);
+            this.txtStacktrace.TabIndex = 4;
+            // 
+            // chkHideSystem
+            // 
+            this.chkHideSystem.AutoSize = true;
+            this.chkHideSystem.Location = new System.Drawing.Point(71, 174);
+            this.chkHideSystem.Name = "chkHideSystem";
+            this.chkHideSystem.Size = new System.Drawing.Size(107, 17);
+            this.chkHideSystem.TabIndex = 3;
+            this.chkHideSystem.Text = "Hide system calls";
+            this.chkHideSystem.UseVisualStyleBackColor = true;
+            this.chkHideSystem.CheckedChanged += new System.EventHandler(this.chkHideSystem_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Stacktrace";
+            // 
+            // lstExceptions
+            // 
+            this.lstExceptions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstExceptions.FormattingEnabled = true;
+            this.lstExceptions.Location = new System.Drawing.Point(6, 19);
+            this.lstExceptions.Name = "lstExceptions";
+            this.lstExceptions.Size = new System.Drawing.Size(970, 147);
+            this.lstExceptions.TabIndex = 0;
+            this.lstExceptions.SelectedIndexChanged += new System.EventHandler(this.lstExceptions_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 436);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Unity Log Parser v0.12";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstExceptions;
+        private System.Windows.Forms.CheckBox chkHideSystem;
+        private System.Windows.Forms.TextBox txtStacktrace;
+    }
+}
+
---
+++ Forms/frmMain.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnityLogParser
+{
+    public partial class frmMain : Form
+    {
+
+        private class LoggedException
+        {
+            public string Exception;
+            public List<string> Stacktrace = new List<string>();
+
+            public LoggedException(string exception)
+            {
+                this.Exception = exception;
+            }
+
+            public long GetHash()
+            {
+                unchecked
+                {
+                    long hash = Exception.GetHashCode();
+                    for (var i = 0; i < Stacktrace.Count; i++)
+                        hash = (hash * 16777619) ^ Stacktrace[i].GetHashCode();
+                    return hash;
+                }
+            }
+
+            public override string ToString()
+            {
+                return Exception;
+            }
+        }
+
+        private static string[] systemAssemblies = new string[] {
+            "  at System.",
+            "  at UnityEngine.",
+            "  at UnityEditor."
+        };
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "Unity Log Files | *.log;*.txt";
+            dlg.InitialDirectory = Path.Combine(GetLocalLowPath(), "Odd Raven Studios");
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = dlg.FileName;
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string line;
+            LoggedException currentException = null;
+            Dictionary<long, LoggedException> exceptions = new Dictionary<long, LoggedException>();
+            lstExceptions.Items.Clear();
+            using (var fs = new FileStream(txtFile.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var fileReader = new StreamReader(fs))
+                {
+                    while ((line = fileReader.ReadLine()) != null)
+                    {
+                        var ex = line.Split(':');
+                        if (ex.Length > 0 && ex[0].EndsWith("Exception"))
+                        {
+                            if (currentException != null)
+                            {
+                                if (!exceptions.ContainsKey(currentException.GetHash()))
+                                {
+                                    exceptions.Add(currentException.GetHash(), currentException);
+                                }
+                            }
+                            currentException = new LoggedException(line);
+                        }
+                        else if (line.StartsWith("  at ") && currentException != null)
+                        {
+                            currentException.Stacktrace.Add(line);
+                        }
+                    }
+                    fileReader.Close();
+                }
+                fs.Close();
+            }
+            foreach(var ex in exceptions)
+            {
+                lstExceptions.Items.Add(ex.Value);
+            }
+            txtStacktrace.Text = "";
+        }
+
+        private void lstExceptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var ex = lstExceptions.SelectedItem as LoggedException;
+            if (ex == null)
+                return;
+            txtStacktrace.Text = ex.ToString() + "\r\n" + string.Join("\r\n", ex.Stacktrace.Where(x => !chkHideSystem.Checked || !(systemAssemblies.Any(a => x.StartsWith(a)))));
+        }
+
+        private void chkHideSystem_CheckedChanged(object sender, EventArgs e)
+        {
+            lstExceptions_SelectedIndexChanged(sender, e);
+        }
+
+        private string GetLocalLowPath()
+        {
+            Guid localLowId = new Guid(@"A520A1A4-1780-4FF6-BD18-167343C5AF16");
+            return GetKnownFolderPath(localLowId);
+        }
+
+        string GetKnownFolderPath(Guid knownFolderId)
+        {
+            IntPtr pszPath = IntPtr.Zero;
+            try
+            {
+                int hr = SHGetKnownFolderPath(knownFolderId, 0, IntPtr.Zero, out pszPath);
+                if (hr >= 0)
+                    return Marshal.PtrToStringAuto(pszPath);
+                throw Marshal.GetExceptionForHR(hr);
+            }
+            finally
+            {
+                if (pszPath != IntPtr.Zero)
+                    Marshal.FreeCoTaskMem(pszPath);
+            }
+        }
+
+        [DllImport("shell32.dll")]
+        static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out IntPtr pszPath);
+
+        private void txtFile_TextChanged(object sender, EventArgs e)
+        {
+            btnRead.Enabled = File.Exists(txtFile.Text);
+        }
+
+    }
+}
